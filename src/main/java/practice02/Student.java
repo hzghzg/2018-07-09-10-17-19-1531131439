40c31d0ed5e65123e35306ed1d64e4fcfd619ee2
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-07-12 08:32:35 +0800

    complete practice01

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,14 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    private  int kclass;
+
+    public Student(String name,int age,int kclass) {
+        super(name,age);
+        this.kclass = kclass;
+    }
+
+    public int getKclass() {
+        return kclass;
+    }
 }
