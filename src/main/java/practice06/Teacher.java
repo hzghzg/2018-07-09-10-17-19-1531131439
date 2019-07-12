499e45677eb893c6baf699568c9e7625d6d80519
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-07-12 09:18:00 +0800

    complete practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,22 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private  int kclass;
+    public Teacher(String name,int age) {
+        super(name,age);
+    }
+
+    public Teacher(String name,int age,int kclass) {
+        super(name,age);
+        this.kclass = kclass;
+    }
+
+    public int getKlass() {
+        return kclass;
+    }
+    @Override
+    public String introduce(){
+        if(this.kclass==0){return super.introduce()+" I am a Teacher. I teach No Class.";}
+        else return super.introduce()+" I am a Teacher. I teach Class 2.";
+    }
 }
